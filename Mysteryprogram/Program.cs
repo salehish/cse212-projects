a4edee83f56d9edc44d96c7b6defd68aabfebdd5
saleh <ntegesaleh@gmail.com>
2026-05-15 14:37:02 +0300

    week02 SimpleQueue

---
+++ Mysteryprogram/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+class D
+{
+    static void Main()
+    {
+        int[] d = R(5);
+        Array.Sort(d);
+        Console.WriteLine("Values: " + string.Join(", ", d));
+        int s = C(d);
+        Console.WriteLine("Total: " + s);
+    }
+
+    static int[] R(int n)
+    {
+        Random r = new Random();
+        int[] d = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            d[i] = r.Next(1, 7);
+        }
+        return d;
+    }
+
+    static int C(int[] d)
+    {
+        int s = 0;
+        Dictionary<int, int> c = new Dictionary<int, int>();
+        foreach (int x in d)
+        {
+            if (c.ContainsKey(x))
+            {
+                c[x]++;
+            }
+            else
+            {
+                c[x] = 1;
+            }
+        }
+        foreach (int v in c.Values)
+        {
+            switch (v)
+            {
+                case 2: s += 10; break;
+                case 3: s += 20; break;
+                case 4: s += 30; break;
+                case 5: s += 40; break;
+            }
+        }
+        return s;
+    }
+}
