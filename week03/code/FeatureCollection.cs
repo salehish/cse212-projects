ebbff5dd1cf65797f389cd686670d585b2e2254f
saleh <ntegesaleh@gmail.com>
2026-05-22 01:46:09 +0300

    week03 SetAndMaps

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,66 @@
+using System.Text.Json.Serialization;
+
 public class FeatureCollection
 {
-    // TODO Problem 5 - ADD YOUR CODE HERE
-    // Create additional classes as necessary
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+
+    [JsonPropertyName("metadata")]
+    public MetadataInfo Metadata { get; set; }
+    [JsonPropertyName("features")]
+    public List<Feature> Features { get; set; }
+
+    public class MetadataInfo
+    {
+        [JsonPropertyName("generated")]
+        public long Generated { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+
+        [JsonPropertyName("api")]
+        public string Api { get; set; }
+
+        [JsonPropertyName("count")]
+        public int Count { get; set; }
+    }
+
+    public class Feature
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("properties")]
+        public Properties Properties { get; set; }
+    }
+
+    public class Properties
+    {
+        [JsonPropertyName("mag")]
+        public double? Mag { get; set; }
+
+        [JsonPropertyName("place")]
+        public string Place { get; set; }
+
+        [JsonPropertyName("time")]
+        public long Time { get; set; }
+
+        [JsonPropertyName("updated")]
+        public long Updated { get; set; }
+
+        [JsonPropertyName("tz")]
+        public int? Tz { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("detail")]
+        public string Detail { get; set; }
+    }
 }
--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -21,8 +21,24 @@ public static class SetsAndMaps
     /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
     public static string[] FindPairs(string[] words)
     {
-        // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        var set = new HashSet<string>(words);
+        var results = new List<string>();
+
+        foreach (var word in words)
+        {
+            string reversed = new string([word[1], word[0]]);
+
+            if (set.Contains(reversed) && word[0] != word[1])
+            {
+                if (string.Compare(word, reversed) < 0)
+                {
+                    results.Add($"{word} & {reversed}");
+                }
+
+            }
+        }
+
+        return results.ToArray();
     }
 
     /// <summary>
@@ -39,10 +55,19 @@ public static class SetsAndMaps
     public static Dictionary<string, int> SummarizeDegrees(string filename)
     {
         var degrees = new Dictionary<string, int>();
+
         foreach (var line in File.ReadLines(filename))
         {
             var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+            string degree = fields[3].Trim();
+            if (degrees.ContainsKey(degree))
+            {
+                degrees[degree]++;
+            }
+            else
+            {
+                degrees[degree] = 1;
+            }
         }
 
         return degrees;
@@ -66,8 +91,43 @@ public static class SetsAndMaps
     /// </summary>
     public static bool IsAnagram(string word1, string word2)
     {
-        // TODO Problem 3 - ADD YOUR CODE HERE
-        return false;
+        word1 = word1.Replace(" ", "").ToLower();
+        word2 = word2.Replace(" ", "").ToLower();
+
+        if (word1.Length != word2.Length) 
+        {
+            return false;
+        }
+
+        var Counts = new Dictionary<char, int>();
+
+        foreach (var c in word1)
+        {
+            if (Counts.ContainsKey(c))
+            {
+                Counts[c]++;
+            }
+            else
+            {
+                Counts[c] = 1;
+            }
+        }
+
+        foreach (char c in word2)
+        {
+            if (!Counts.ContainsKey(c))
+            {
+                return false;
+            }
+            Counts[c]--;
+
+            if (Counts[c] < 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /// <summary>
@@ -96,11 +156,19 @@ public static class SetsAndMaps
 
         var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
 
-        // TODO Problem 5:
+        List<string> summaries = new();
+
+        foreach (var feature in featureCollection.Features)
+        {
+            string place = feature.Properties.Place;
+            string mag = feature.Properties.Mag?.ToString() ?? "known";
+
+            summaries.Add($"{place} - Mag  {mag}");
+        }
         // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
         // on those classes so that the call to Deserialize above works properly.
         // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
         // 3. Return an array of these string descriptions.
-        return [];
+        return summaries.ToArray();
     }
 }
