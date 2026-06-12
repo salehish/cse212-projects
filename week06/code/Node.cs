da17a6dd7607721a4339e4222ced8f36add96299
saleh <ntegesaleh@gmail.com>
2026-06-12 21:36:24 +0300

    Completed Week 06 Trees assignment

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -10,36 +10,62 @@ public class Node
     }
 
     public void Insert(int value)
-    {
-        // TODO Start Problem 1
+{
+    // TODO Start Problem 1
 
-        if (value < Data)
-        {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
-            else
-                Left.Insert(value);
-        }
-        else
-        {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
-        }
+    if (value == Data)
+    {
+        return;
     }
+
+    if (value < Data)
+    {
+        // Insert to the left
+        if (Left is null)
+            Left = new Node(value);
+        else
+            Left.Insert(value);
+    }
+    else
+    {
+        // Insert to the right
+        if (Right is null)
+            Right = new Node(value);
+        else
+            Right.Insert(value);
+    }
+}
 
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+
+        if (value == Data)
+            return true;
+
+        if (value < Data)
+        {
+            if (Left is null)
+                return false;
+
+            return Left.Contains(value);
+        }
+        else
+        {
+            if (Right is null)
+                return false;
+
+            return Right.Contains(value);
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+
+        int leftHeight = Left == null ? 0 : Left.GetHeight();
+        int rightHeight = Right == null ? 0 : Right.GetHeight();
+
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
